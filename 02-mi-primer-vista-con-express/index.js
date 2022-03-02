6f956a55afbc68a6cc78ba71fa8b959545325761
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-02 18:52:32 +0100

    Creating a Server

---
+++ 02-mi-primer-vista-con-express/index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const port = 5000
+
+const server = http.createServer((req, res) => {
+	res.end('esta es la respuesta 🚀 ')
+})
+
+server.listen(port, () => console.log('funcionando 😇 '))
